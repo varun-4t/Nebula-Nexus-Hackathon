c11a826b506f9d9c2dba4935946016373e8f393f
Varun Tahiliani <gabbarboss228@gmail.com>
2025-07-17 22:11:04 +0530

    Zoomed the bg

--- src/App.js
+++ src/App.js
@@ -7,15 +7,20 @@ import Spline from './components/SplineViewer';
 function App() {
   return (
     <div className="relative min-h-screen flex items-center justify-center overflow-hidden">
-      {/* Background */}
-      <div className="absolute inset-0 z-0">
-        <Spline />
+
+      {/* Spline bg*/}
+      <div className="absolute inset-0 z-10 pointer-events-none">
+        <div className="scale-125 w-full h-full">
+          <Spline />
+        </div>
       </div>
 
+
       {/* Foreground Content */}
-      <div className="relative z-10">
+      <div className="relative z-20">
         <GlassCard />
       </div>
+
     </div>
   );
 }
