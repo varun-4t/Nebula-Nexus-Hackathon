6705b18d8e27a6fb8ca1336117bba03a84c92f6d
Varun Tahiliani <gabbarboss228@gmail.com>
2025-07-20 13:40:35 +0530

    updated with mars code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import logo from './logo.svg';
 import './index.css';
 import './App.css';
-import GlassCard from './components/GlassCard';
-import Spline from './components/SplineViewer';
+import GlassCard from './components/mars';
 
 function App() {
   return (
     <div className="relative min-h-screen flex items-center justify-center overflow-hidden">
 
-      {/* Spline bg*/}
-      <div className="absolute inset-0 z-10 pointer-events-none">
-        <div className="scale-125 w-full h-full">
-          <Spline />
-        </div>
-      </div>
-
-
-      {/* Foreground Content */}
       <div className="relative z-20">
         <GlassCard />
       </div>
